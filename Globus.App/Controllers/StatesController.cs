139cfb04b6af09295763860dadfd12e1f9d65908
Damilola Odunbaku <me@damilola.io>
2022-03-27 20:39:36 +0100

    add controller for states and lga information retrieval

---
+++ Globus.App/Controllers/StatesController.cs
@@ -0,0 +1,34 @@
+﻿using Globus.App.Business.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Globus.App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatesController : ControllerBase
+    {
+        private readonly ILogger<StatesController> _logger;
+        private readonly NigerianStatesService _nigerianStatesService;
+
+        public StatesController(ILogger<StatesController> logger,
+            NigerianStatesService nigerianStatesService)
+        {
+            _logger = logger;
+            _nigerianStatesService = nigerianStatesService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<string>> GetStates()
+        {
+            return new JsonResult(_nigerianStatesService.GetAllStates());
+        }
+
+        [HttpGet("lga")]
+        public ActionResult GetLgasForState(string state)
+        {
+            return new JsonResult(_nigerianStatesService.GetLgaForState(state));
+        }
+    }
+}
