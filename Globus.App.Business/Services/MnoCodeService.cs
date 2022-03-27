0bc7021726e647c121bfddbf4faa399dfc24bb9b
Damilola Odunbaku <me@damilola.io>
2022-03-27 16:00:34 +0100

    add services for MNO Code validation and State/LGA reporting

---
+++ Globus.App.Business/Services/MnoCodeService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globus.App.Business.Services
+{
+    public class MnoCodeService
+    {
+        private readonly ILogger<MnoCodeService> _logger;
+        string _mnoCodesFileName;
+        HashSet<string> _hashSet;
+        public MnoCodeService(ILogger<MnoCodeService> logger,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _mnoCodesFileName = configuration["mnoCodesFileName"];
+            LoadMnoCodes();
+
+        }
+
+        private void LoadMnoCodes()
+        {
+            try
+            {
+                _hashSet = new HashSet<string>();
+                string filePath = Path.Combine(Environment.CurrentDirectory, _mnoCodesFileName);
+                if (File.Exists(filePath))
+                {
+                    using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader(fileStream))
+                        {
+                            while (!streamReader.EndOfStream)
+                            {
+                                string line = streamReader.ReadLine();
+                                if (line != null)
+                                {
+                                    _hashSet.Add(line.Split(",")[0]);
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error occured while loading Mobile network operator codes");
+                throw;
+            }
+        }
+
+        public bool IsValidMnoCode(string code)
+        {
+            return _hashSet.Contains(code);
+        }
+    }
+}
---
+++ Globus.App.Business/Services/NigerianStatesService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globus.App.Business.Services
+{
+    public class NigerianStatesService
+    {
+        string _statesFileName;
+        Dictionary<string, List<string>> _states;
+        private readonly ILogger<NigerianStatesService> _logger;
+
+        public NigerianStatesService(
+            ILogger<NigerianStatesService> logger,
+            IConfiguration configuration)
+        {
+            _statesFileName = configuration["statesFileName"];
+            _logger = logger;
+            LoadStates();
+        }
+
+        private void LoadStates()
+        {
+            try
+            {
+                string filePath = Path.Combine(Environment.CurrentDirectory, _statesFileName);
+
+                if (File.Exists(filePath))
+                {
+                    _states = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(File.ReadAllText(filePath));
+                }
+                else
+                {
+                    _states = new Dictionary<string, List<string>>();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error occured while reading state and LGA information");
+                throw;
+            }
+        }
+
+        public List<string> GetAllStates()
+        {
+            return _states.Keys.ToList();
+        }
+
+        public List<string> GetLgaForState(string state)
+        {
+            var lgas = _states.
+                        Where(k => k.Key.Equals(state, StringComparison.OrdinalIgnoreCase))
+                        .SelectMany(k => k.Value)
+                        .ToList();
+
+            return lgas;
+        }
+    }
+}
