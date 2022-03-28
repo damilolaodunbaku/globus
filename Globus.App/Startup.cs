ffb343ff91d4eec84980e4f13e8e8ab33c1bfa70
Damilola Odunbaku <me@damilola.io>
2022-03-28 09:36:04 +0100

    update swagger settings

--- Globus.App/Startup.cs
+++ Globus.App/Startup.cs
@@ -83,7 +83,11 @@ namespace Globus.App
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Globus.App v1"));
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Globus.App v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
